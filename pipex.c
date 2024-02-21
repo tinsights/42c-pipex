230535603293f7c838440cc82a292a44071e4673
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-02-21 20:13:55 +0800

    lol norm

--- pipex.c
+++ pipex.c
@@ -12,34 +12,33 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 #include "libft.h"
 
-
-int	main(int ac, char **av, char ** envp)
+int	main(int ac, char **av, char **envp)
 {
+	char	*file;
+	char	*cmd;
+	char	*leftpipe;
+	char	*bashcmd;
+	
+
 	if (ac != 5)
-		return printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
-
-
-	char *bin_path = "/bin/bash";
-
-	char *file = ft_strjoin(av[1], " ");
-	char *cmd = ft_strjoin(file, av[2]);
-	char *leftpipe = ft_strjoin("< ", cmd);
+		return (printf("Usage: ./pipex file1 cmd1 cmd2 file2\n"));
+	file = ft_strjoin(av[1], " ");
+	cmd = ft_strjoin(file, av[2]);
+	leftpipe = ft_strjoin("< ", cmd);
 	free(file);
 	free(cmd);
-
 	file = ft_strjoin(av[3], " > ");
 	cmd = ft_strjoin(file, av[4]);
 	free(file);
-
 	file = ft_strjoin(" | ", cmd);
 	free(cmd);
-
-	char *bashcmd = ft_strjoin(leftpipe, file);
+	bashcmd = ft_strjoin(leftpipe, file);
 	free(leftpipe);
-
-	char *bashargs[] = { bin_path, "-c", bashcmd, NULL};
-	execve(bin_path, bashargs, envp);
+	char	*bashargs[] = {"/bin/bash", "-c", bashcmd, NULL};
+	execve("/bin/bash", bashargs, envp);
 	perror("");
+	return (errno);
 }
