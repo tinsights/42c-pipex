cff6f423b2d6ccff868f31884bf9c094d319681e
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-02-23 19:40:00 +0800

    eh whatever

--- pipex.c
+++ pipex.c
@@ -11,63 +11,64 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "libft.h"
 
 
-int	main(int ac, char **av, char ** envp)
+int	main(int ac, char **av)
 {
 	if (ac != 3)
 		return printf("Usage: ./pipex file1 cmd1\n");
 
 
-	char **PATHS;
+	// char **PATHS;
 
 	char *file1;
-	// char *file2;
-	char **cmd1;
-	// char *cmd2;
-	int 	i;
+	char *cmd1;
+	int		p_fd[2];
 
 	file1 = av[1];
-	cmd1 = ft_split(av[2], ' ');
-	printf("< %s %s\n", file1, cmd1[0]);
-	// printf("cat %s | %s\n", file1, cmd1[0]);
+	cmd1 = av[2];
 
-	while (*envp)
+	if (pipe(p_fd) < 0)
+		return (-1);
+
+	int pid = fork();
+	if (pid  == -1)
+		return (-2);
+
+	if (pid == 0)
 	{
-		if (!ft_strncmp(*envp, "PATH", 4))
-		{
-			PATHS = ft_split(*envp + 5, ':');
-		}
-		envp++;
-	}
+		// child process
+		//p_fd[0] read
+		//p_fd[1] write
+		write(1, "child\n", 6);
 
-	int j = 0;
-	while (PATHS[j])
+		// child process
+		int fd = open(file1, O_RDONLY | O_CREAT);
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+
+		close(p_fd[0]); // we are never reading
+		dup2(p_fd[1], STDOUT_FILENO);
+		execlp("tr" ,"tr", "-d", "l", NULL);
+		close(p_fd[1]);
+	}
+	else
 	{
-		char *cmd = ft_strjoin("/", cmd1[0]);
-		char *binpath = ft_strjoin(PATHS[j], cmd);
-		free(cmd);
-		free(PATHS[j]);
-		PATHS[j] = binpath;
-		if (!access(PATHS[j], X_OK))
-			break;
-		j++;
+		// parent process
+		close(p_fd[1]); // we are never writing
+		write(1, "parent\n", 8);
+		dup2(p_fd[0], STDIN_FILENO);
+		close(p_fd[0]);
+		wait(NULL);
+		execlp("cat" ,"cat", NULL);
+
 	}
+	close(p_fd[1]);
+	close(p_fd[0]);
 
-	char *binaryPath = PATHS[j];
-  	execve(binaryPath, cmd1, envp);
-  	perror("");
-
-
-		i = 0;
-	while (PATHS[i])
-		free(PATHS[i++]);
-	free (PATHS);
-	
-	i = 0;
-	while (cmd1[i])
-		free(cmd1[i++]);
-	free(cmd1);
 }
