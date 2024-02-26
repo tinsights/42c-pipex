234f28bf1c89e17d2c94ec34dd47b3da18258448
mister T <aj.tinaes@icloud.com>
2024-02-26 09:21:18 +0800

    2 way pipe

--- pipex.c
+++ pipex.c
@@ -15,6 +15,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 // #include "libft.h"
 
 
@@ -29,15 +30,17 @@ int	main(int ac, char **av)
 	char *file1;
 	char *cmd1;
 	int		p_fd[2];
+	int		p2_fd[2];
+	time_t 	t;
 
 	file1 = av[1];
 	cmd1 = av[2];
 
+	srand((unsigned) time(&t));
 	if (pipe(p_fd) < 0)
 		return (-1);
-
-	// int fd = open("file2", O_RDWR | O_CREAT, 0777);
-	// dup2(fd, p_fd[0]);
+	if (pipe(p2_fd) < 0)
+		return (-1);
 
 	int pid = fork();
 	if (pid  == -1)
@@ -46,39 +49,31 @@ int	main(int ac, char **av)
 	if (pid == 0)
 	{
 		// child process
-		//p_fd[0] read
-		//p_fd[1] write
-		write(1, "child\n", 6);
+		int i;
+		int recvd;
 
-		// child process
-		int fd = open(file1, O_RDONLY | O_CREAT);
-		dup2(fd, STDIN_FILENO);
-		close(fd);
+		i = rand() % 42;
+		printf("child generated %i\n", i);
 
-		close(p_fd[0]); // we are never reading
-		dup2(p_fd[1], STDOUT_FILENO);
-		sleep(3);
-		execlp("tr" ,"tr", "-d", "l", NULL);
-		close(p_fd[1]);
+		write(p_fd[1], &i, 4);
+		read(p_fd[0], &recvd, 4);
+		printf("child received %i\n", recvd);
+
 	}
 	else
 	{
 		// parent process
-		close(p_fd[1]); // we are never writing
-		write(1, "parent\n", 8);
+		int i;
+		int recvd;
 
-		// dup2(p_fd[0], STDIN_FILENO);
-		// close(p_fd[0]);
-		char c;
-		int nb = read(p_fd[0], &c, 1);
-		write(1, &nb, 4);
-		while (nb > 0)
-		{
-			write(1, &c, 1);
-			nb = read(p_fd[0], &c, 1);
+		read(p_fd[0], &recvd, 4);
+		printf("parent received %i\n", recvd);
 
-		}
-		wait(NULL);
+		i = rand() % recvd;
+		printf("parent generated %i\n", i);
+
+		write(p2_fd[1], &i, 4);
+
 		
 	}
 	close(p_fd[1]);
