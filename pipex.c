324720d5c77b9d0c139a25269a866dc810addbc3
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-02-27 15:32:07 +0800

    pre norm

--- pipex.c
+++ pipex.c
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include "libft.h"
 
 
@@ -36,7 +37,6 @@ char *check_valid_cmd(char **PATHS, char *cmd)
 	return binpath;
 }
 
-
 /*
 TODOS:
 1) Base case for recursion: if last command reached
@@ -44,12 +44,13 @@ TODOS:
 3) write to file prior to base case
 i.e. if next cmd is last cmd, redirect stdout to file.
 */
-void run_child_process(char **PATHS, char **cmd, char **envp)
+void run_child_process(char **PATHS, char ***cmds, char **envp)
 {
 	int p_fd[2];
 	pipe(p_fd); // error checking later
-	int pid = fork();
 	char *binpath;
+	char **cmd = cmds[0];
+	int pid = fork();
 
 	if (pid == 0)
 	{	// child
@@ -79,36 +80,52 @@ void run_child_process(char **PATHS, char **cmd, char **envp)
 
 		// int fd = open(file2, O_WRONLY | O_TRUNC | O_CREAT, 0777);
 
-		dup2(p_fd[0], STDIN_FILENO);
-		close(p_fd[0]);
+
 
 		// dup2(fd, STDOUT_FILENO);
 		// close(fd);
+		dup2(p_fd[0], STDIN_FILENO);
+		close(p_fd[0]);
+		wait(NULL);
+		if (*(++cmds))
+		{
 
-		run_child_process(PATHS, cmd, envp);
+			run_child_process(PATHS, cmds, envp);
+		}
+		else
+		{
+			return ;
+		}
 	}
+
 }
 
 
 int	main(int ac, char **av, char ** envp)
 {
-	if (ac < 5)
+	if (ac != 5)
 		return printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
 
 	char **PATHS;
 
 	char *file1;
 	char *file2;
-	char **cmd1;
-	char **cmd2;
-
-	char *binpath;
+	char ***cmds;
 
 	file1 = av[1];
 	file2 = av[4];
-	cmd1 = ft_split(av[2], ' ');
-	cmd2 = ft_split(av[3], ' ');
 
+	int num_of_cmds = ac - 3;
+	cmds = ft_calloc(num_of_cmds + 1, sizeof(char *));
+	cmds[num_of_cmds] = NULL;
+
+	int i = 0;
+	while (i < num_of_cmds)
+	{
+		char **cmd = ft_split(av[i + 2], ' ');
+		cmds[i] = cmd;
+		i++;
+	}
 
 	while (*envp)
 	{
@@ -119,54 +136,19 @@ int	main(int ac, char **av, char ** envp)
 		envp++;
 	}
 
-	
-
 	// open file1, replace with stdin
 	int fd = open(file1, O_RDWR | O_CREAT, 0777);
 	dup2(fd, STDIN_FILENO);
 	close(fd);
 
-	int p_fd[2];
-	pipe(p_fd); // error checking later
-	int pid = fork();
+	run_child_process(PATHS, cmds, envp);
 
-	if (pid == 0)
-	{	// child
-		close(p_fd[0]); // we are never reading
+	fd = open(file2, O_WRONLY | O_TRUNC | O_CREAT, 0777);
 
-		//check the command is valid
-		binpath = check_valid_cmd(PATHS, cmd1[0]);
-		if (!binpath)
-			printf("%s: command not found\n", cmd1[0]);
+	char c;
+	while(read(STDIN_FILENO, &c, 1))
+		write(fd, &c, 1);
 
-		// output goes to pipe instead of stdout
-		dup2(p_fd[1], STDOUT_FILENO);
-		close(p_fd[1]);
 
-		//execute
-		execve(binpath, cmd1, envp);
-		perror("");
-	}
-	else
-	{	// PARENT PROCESS
 
-		close(p_fd[1]); // we are never writing
-
-		// binpath = check_valid_cmd(PATHS, cmd2[0]);
-		// if (!binpath)
-		// 	printf("%s: command not found\n", cmd2[0]);
-
-		// int fd = open(file2, O_WRONLY | O_TRUNC | O_CREAT, 0777);
-
-		dup2(p_fd[0], STDIN_FILENO);
-		close(p_fd[0]);
-
-		// dup2(fd, STDOUT_FILENO);
-		// close(fd);
-
-		run_child_process(PATHS, cmd2, envp);
-		// execve(binpath, cmd2, envp);
-		// perror("");
-
-	}
 }
