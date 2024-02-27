434eb17d7b77d4c5b6d54fc62240cae3ff86ac1e
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-02-27 17:35:02 +0800

    pre norm

--- pipex.c
+++ pipex.c
@@ -17,138 +17,128 @@
 #include <sys/wait.h>
 #include "libft.h"
 
-
-char *check_valid_cmd(char **PATHS, char *cmd)
+typedef struct s_params
 {
-	int j = 0;
-	char *binpath;
-	while (PATHS[j])
+	int		num_cmds;
+	char	**paths;
+	char	***cmds;
+}	t_params;
+
+char	*check_valid_cmd(char **paths, char *cmd)
+{
+	int		j;
+	char	*binpath;
+	char	*cmdpath;
+
+	j = 0;
+	while (paths[j])
 	{
-		char *cmdpath = ft_strjoin("/", cmd);
-		binpath = ft_strjoin(PATHS[j], cmdpath);
+		cmdpath = ft_strjoin("/", cmd);
+		binpath = ft_strjoin(paths[j], cmdpath);
 		free(cmdpath);
 		if (!access(binpath, X_OK))
-			break;
+			break ;
 		free(binpath);
 		j++;
 	}
-	if (PATHS[j] == NULL)
-		return NULL;
-	return binpath;
+	if (paths[j] == NULL)
+		return (NULL);
+	return (binpath);
 }
 
-/*
-TODOS:
-1) Base case for recursion: if last command reached
-2) create array of commands in main
-3) write to file prior to base case
-i.e. if next cmd is last cmd, redirect stdout to file.
-*/
-void run_child_process(char **PATHS, char ***cmds, char **envp)
+void	run_child_process(char **paths, char ***cmds)
 {
-	int p_fd[2];
-	pipe(p_fd); // error checking later
-	char *binpath;
-	char **cmd = cmds[0];
-	int pid = fork();
+	int		p_fd[2];
+	pipe(p_fd);
+	char	*binpath;
+	char	**cmd = cmds[0];
+	int		pid = fork();
 
 	if (pid == 0)
-	{	// child
+	{
 		close(p_fd[0]); // we are never reading
-
-		//check the command is valid
-		binpath = check_valid_cmd(PATHS, cmd[0]);
+		binpath = check_valid_cmd(paths, cmd[0]);
 		if (!binpath)
 			printf("%s: command not found\n", cmd[0]);
-
-		// output goes to pipe instead of stdout
 		dup2(p_fd[1], STDOUT_FILENO);
 		close(p_fd[1]);
-
-		//execute
-		execve(binpath, cmd, envp);
+		execve(binpath, cmd, NULL);
 		perror("");
 	}
 	else
-	{	// PARENT PROCESS
-
-		close(p_fd[1]); // we are never writing
-
-		// binpath = check_valid_cmd(PATHS, cmd2[0]);
-		// if (!binpath)
-		// 	printf("%s: command not found\n", cmd2[0]);
-
-		// int fd = open(file2, O_WRONLY | O_TRUNC | O_CREAT, 0777);
-
-
-
-		// dup2(fd, STDOUT_FILENO);
-		// close(fd);
+	{
+		close(p_fd[1]);
 		dup2(p_fd[0], STDIN_FILENO);
 		close(p_fd[0]);
 		wait(NULL);
 		if (*(++cmds))
-		{
-
-			run_child_process(PATHS, cmds, envp);
-		}
+			run_child_process(paths, cmds);
 		else
-		{
 			return ;
-		}
 	}
+}
 
+void	free_memory(char ***cmds, char **paths)
+{
+	int i;
+	
+	i = 0;
+	while (cmds[i])
+	{
+		char **cmd = cmds[i];
+		int j = -1;
+		while (cmd[++j])
+			free(cmd[j]);
+		free(cmd);
+		i++;
+	}
+	free(cmds);
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
 }
 
 
-int	main(int ac, char **av, char ** envp)
+int init_data(int ac, char **av, t_params *params, char **envp)
 {
-	if (ac != 5)
-		return printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
+	int i;
 
-	char **PATHS;
+	params->num_cmds = ac - 3;
+	params->cmds = ft_calloc(params->num_cmds + 1, sizeof(char *));
+	params->cmds[params->num_cmds] = NULL;
 
-	char *file1;
-	char *file2;
-	char ***cmds;
-
-	file1 = av[1];
-	file2 = av[4];
-
-	int num_of_cmds = ac - 3;
-	cmds = ft_calloc(num_of_cmds + 1, sizeof(char *));
-	cmds[num_of_cmds] = NULL;
-
-	int i = 0;
-	while (i < num_of_cmds)
-	{
-		char **cmd = ft_split(av[i + 2], ' ');
-		cmds[i] = cmd;
-		i++;
-	}
+	i = -1;
+	while (++i < params->num_cmds)
+		params->cmds[i] = ft_split(av[i + 2], ' ');
 
 	while (*envp)
 	{
 		if (!ft_strncmp(*envp, "PATH", 4))
 		{
-			PATHS = ft_split(*envp + 5, ':');
+			params->paths = ft_split(*envp + 5, ':');
+			break ;
 		}
 		envp++;
 	}
+	return (1);
+}
 
-	// open file1, replace with stdin
-	int fd = open(file1, O_RDWR | O_CREAT, 0777);
+int	main(int ac, char **av, char ** envp)
+{
+	char c;
+	int fd;
+	t_params params;
+
+	if (ac < 5)
+		return printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
+	init_data(ac, av, &params, envp);
+	int fd = open(av[1], O_RDWR | O_CREAT, 0777);
 	dup2(fd, STDIN_FILENO);
 	close(fd);
-
-	run_child_process(PATHS, cmds, envp);
-
-	fd = open(file2, O_WRONLY | O_TRUNC | O_CREAT, 0777);
-
-	char c;
+	run_child_process(params.paths, params.cmds);
+	fd = open(av[ac - 1], O_WRONLY | O_TRUNC | O_CREAT, 0777);
 	while(read(STDIN_FILENO, &c, 1))
 		write(fd, &c, 1);
-
-
-
+	free_memory(params.cmds, params.paths);
 }
